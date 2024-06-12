c58c2b722c0a12bf84057a9fa60b12648862ad09
Dipak <chdepz@gmail.com>
2024-06-12 14:17:29 +0545

    fixes

--- src/Components/Allprojects/Allprojects.jsx
+++ src/Components/Allprojects/Allprojects.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./Allprojects.css"
 import Contact from '../Contact/Contact'
+import allprojects from '../../Data/allprojects'
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 function Allprojects() {
+  const projectRef = useRef(null)
+  useGSAP(() => {
+    const elements = projectRef.current.querySelectorAll(".allproject");
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: projectRef.current,
+            start: "top center",
+            end: "bottom center",
+            scrub: true,
+          
+        }
+    });
+
+    tl.from(elements, {
+      y: 100,
+        // scale: 1.1,
+        opacity: 0,
+        duration: 0.6,
+        stagger: 0.3,
+        ease: "power4.out",
+    });
+
+    // Clean up on component unmount
+    return () => {
+        if (tl.scrollTrigger) tl.scrollTrigger.kill();
+        tl.kill();
+    };
+}, []);
   return (
     <div>
+       <div ref={projectRef} className="allprojects">
+      <h1> 🔰All projects🔰</h1>
+      <h5>I am passionate about building projects to refine my Development skills. <br /> Explore this archive to see the diverse range of my work.</h5>
+      <div className="allprojects-container">
+        {allprojects.map((project, index) => (
+          <div className="allproject" key={index}>
+            <div className="allproject-img">
+              <img src={project.image} alt="" />
+            </div>
+            <div className="allproject-desc">
+              <h2>{project.title}⚡ </h2>
+              <p>
+                <span>OVERVIEW✍: </span> {project.Description}
+              </p>
+
+              <div className="links">
+                <h3>Project Links</h3>
+                <div className="btn">
+                  <button>
+                    {" "}
+                    <a href={project.github} target="_blank">
+                      Github <i class="fa-brands fa-github"></i>
+                    </a>
+                  </button>
+                  <button>
+                    {" "}
+                    <a href={project.live} target="_blank">
+                      Live link <i class="fa-regular fa-star"></i>
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
         <Contact />
     </div>
   )
---
+++ src/Data/allprojects.js
@@ -0,0 +1,35 @@
+import hamrobazar from "./project_img/Hamrobazar.png"
+
+const allprojects = [
+    {
+        title: "Hamro Bazar",
+        github: "https://github.com/chdepz-dev/hamro-bazar-ReactCommerce",
+        live: "",
+        image: hamrobazar,
+        Description: "Developed a dynamic e-commerce web application using React for the front-end and MongoDB for the database. This project demonstrates my ability to create a full-stack application with a seamless shopping experience, including product listings, user authentication, and order management.",
+    },
+    {
+        title: "Hamro Bazar",
+        github: "https://github.com/chdepz-dev/hamro-bazar-ReactCommerce",
+        live: "",
+        image: hamrobazar,
+        Description: "Developed a dynamic e-commerce web application using React for the front-end and MongoDB for the database. This project demonstrates my ability to create a full-stack application with a seamless shopping experience, including product listings, user authentication, and order management.",
+    },
+    {
+        title: "Hamro Bazar",
+        github: "https://github.com/chdepz-dev/hamro-bazar-ReactCommerce",
+        live: "",
+        image: hamrobazar,
+        Description: "Developed a dynamic e-commerce web application using React for the front-end and MongoDB for the database. This project demonstrates my ability to create a full-stack application with a seamless shopping experience, including product listings, user authentication, and order management.",
+    },
+    {
+        title: "Hamro Bazar",
+        github: "https://github.com/chdepz-dev/hamro-bazar-ReactCommerce",
+        live: "",
+        image: hamrobazar,
+        Description: "Developed a dynamic e-commerce web application using React for the front-end and MongoDB for the database. This project demonstrates my ability to create a full-stack application with a seamless shopping experience, including product listings, user authentication, and order management.",
+    },
+    
+]
+
+export default allprojects;
